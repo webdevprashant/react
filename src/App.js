749d055f63961e1008b9675154e07c2651e999c5
Prashant Kumar <prashantkumar69328@gmail.com>
2023-11-27 23:00:48 +0530

    React tut start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+const user = {
+  name: "Hedy Lamarr",
+  imageUrl: "https://i.imgur.com/yXOvdOSs.jpg",
+  imageSize: 90,
+};
 
 function App() {
+  return <div>This is div</div>;
+}
+
+function App2() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Welcome to my app</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={"Photo of " + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize,
+        }}
+      />
+      <button>I'm a button</button>
     </div>
   );
 }
 
-export default App;
+let isLoggedIn = true;
+let content;
+const products = [
+  { title: "Cabbage", isFruit: true, id: 1 },
+  { title: "Garlic", isFruit: false, id: 2 },
+  { title: "Apple", isFruit: true, id: 3 },
+];
+
+function App3() {
+  // if (isLoggedIn) {
+  //   content = <App />;
+  // } else {
+  //   content = <App2 />;
+  // }
+  // return <div>{content}</div>;
+  // return <div>{isLoggedIn ? <App /> : <App2 />}</div>;
+  return <div>{isLoggedIn && <App2 />}</div>;
+}
+
+const listItems = products.map((item) => (
+  <li key={item.id} style={{ color: item.isFruit ? "magenta" : "darkgreen" }}>
+    {item.title}
+  </li>
+));
+function App4() {
+  return <ul>{listItems}</ul>;
+}
+
+function App5() {
+  function handleClick() {
+    alert("You clicked me!");
+  }
+
+  return <button onClick={handleClick}>Click me</button>;
+}
+export { App3, App4, App5 };
+// export { App, App2 };
