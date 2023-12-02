8b45e0ca248dc4cd8fe67ec4fa6c3807188e0e3c
Prashant Kumar <prashantkumar69328@gmail.com>
2023-12-03 01:11:19 +0530

    Tic tac toe react

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM  from "react-dom/client";
+import "./index.css";
+
+import Board from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <React.StrictMode>
+        <Board />
+    </React.StrictMode>
+);
+    
+reportWebVitals();
